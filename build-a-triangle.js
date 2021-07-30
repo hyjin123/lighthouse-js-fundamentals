0b9af93105ab3a3e50ed4861ee4c8d45df366782
Sean Jin <seanhoyeonjin@gmail.com>
2021-07-30 02:04:20 +0000

    few tests

--- build-a-triangle.js
+++ build-a-triangle.js
@@ -9,7 +9,7 @@ function makeLine(length) {
 
 // your code goes here.  Make sure you call makeLine() in your own code.
 function buildTriangle(triangle) {
-  let firstLines = ""
+  let firstLines = "";
   for (let x = 1; x <= triangle; x++) {
     firstLines += makeLine(x);
   }
@@ -18,3 +18,23 @@ function buildTriangle(triangle) {
 
 // test your code by uncommenting the following line
 console.log(buildTriangle(10));
+
+/*
+
+// Function expression that assigns the function displayFavorite 
+// to the variable favoriteMovie
+var favoriteMovie = function displayFavorite(movieName) {
+  console.log("My favorite movie is " + movieName);
+};
+
+// Function declaration that has two parameters: a function for displaying
+// a message, along with a name of a movie
+function movies(messageFunction, name) {
+  messageFunction(name);
+}
+
+// Call the movies function, pass in the favoriteMovie function and name of movie
+movies(favoriteMovie, "Finding Nemo");
+
+*/
+
