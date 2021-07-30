395dc20121626edae234d46b049be396ce6b66d0
Sean Jin <seanhoyeonjin@gmail.com>
2021-07-30 19:55:00 +0000

    functions age calculator challenge

---
+++ functions-age-calculator.js
@@ -0,0 +1,9 @@
+// function that calculates the user's age based on their birth year
+
+function ageCalculator(name, yearOfBirth, currentYear) {
+  let age = currentYear - yearOfBirth;
+  return name + " is " + age + " years old.";
+}
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
