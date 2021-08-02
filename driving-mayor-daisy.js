e7a65c152b080481e7ad095eaafce6f08d491ce0
Sean Jin <seanhoyeonjin@gmail.com>
2021-08-02 23:31:52 +0000

    Driving Mayor Daisy Challenge

---
+++ driving-mayor-daisy.js
@@ -0,0 +1,28 @@
+//Build a function to monitor how often cars pass by, and track their speeds
+
+function carPassing(cars, speed) {
+  cars.push({
+    time: Date.now(),
+    speed: speed
+  })
+  return cars;
+}
+
+const cars = [
+  {
+    time: 1568329654807,
+    speed: 40,
+  },
+  {
+    time: 1568329821632,
+    speed: 42,
+  },
+  {
+    time: 1568331115463,
+    speed: 35
+  }
+]
+
+const speed = 38;
+
+console.log(carPassing(cars, speed));
