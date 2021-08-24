1ca8e808ae571d5a6d5e8e9905f939497fddf5e1
Sean Jin <seanhoyeonjin@gmail.com>
2021-08-24 15:00:55 +0000

    Merge Arrays Challenge

---
+++ mergeArrays.js
@@ -0,0 +1,19 @@
+const merge = function (array1, array2) {
+  for (let i = 0; i < array2.length; i++) {
+    array1.push(array2[i]);
+  }
+  array1.sort(function (a, b) {
+    if (a < b) {
+      return -1;
+    }
+    if (a > b) {
+      return 1;
+    }
+    return 0;
+  });
+  return array1;
+}
+
+console.log(merge([4, 5, 6], [1, 2, 3, 4]), "=?", [1, 2, 3, 4, 4, 5, 6]);
+console.log(merge([4], [2, 5, 8]), "=?", [2, 4, 5, 8]);
+console.log(merge([1, 2, 6], []), "=?", [1, 2, 6]);
